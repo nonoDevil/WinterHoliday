fac351735b00ae4c99e56b011a2ee42a5036da3e
chxccc <chenhexin1993@gmail.com>
2013-01-16 18:40:31 +0800

    根据用户输入的分子和分母，把分数转化为标准最简形式（包括约分以及将负号的位置统一），方便日后的分数计算

---
+++ fract1.cpp
@@ -0,0 +1,96 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+/*根据用户输入的分子和分母，将分数约分，方便日后的计算*/
+class fraction{
+
+	private:
+		int num,  den;
+	public:
+		void set(int n, int d)
+		{
+			num = n;
+			den = d;
+			normalize();
+		}
+		int get_num()
+		{
+			return num;
+		}
+		int get_den()
+		{
+			return den;
+		}
+	private:
+		void normalize();
+		int gcf(int a, int b);
+		int lcm(int a, int b);
+
+
+};
+
+void fraction::normalize()
+{
+	if(den == 0 || num == 0){
+	
+		num = 0;
+		den = 1;
+	}
+	if(den < 0){
+	
+		num *= -1;
+		den *= -1;
+	}
+	int n = gcf(num, den);
+	num /= n;
+	den /= n;
+}
+
+int fraction::gcf(int a, int b)
+{
+	if(b == 0){
+	
+		return abs(a);
+	}
+	return gcf(b, a%b);
+
+}
+
+int fraction::lcm(int a, int b)
+{
+	int n = gcf(a, b);
+	return a / n * b;
+}
+
+int main(void)
+{
+	fraction fract;
+	string str;
+	int a, b;
+	while(true){
+	
+		cout << "enter 分子:" << endl;
+		cin >> a;
+		cout << "enter 分母:" << endl;
+		cin >> b;
+
+		fract.set(a, b);
+		cout << "化简后:" ;
+		cout << "分子是: " << fract.get_num() << endl;
+		cout << "分母是: " << fract.get_den()<< endl;
+
+		cout << "继续？" << endl;
+		cin >> str[0] ;    //有点大材小用
+		if(str[0] != 'y' && str[0] != 'Y'){
+		
+			break;
+		}
+
+	}
+
+	
+
+	return 0;
+}
