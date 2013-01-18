661529561b952fc72d4ddb008c2e95de87b87f07
Jiajie.Chen <linux.kakit@gmail.com>
2013-01-18 19:24:09 +0800

    add first cpp file

---
+++ mr_beaver/hello.cpp
@@ -0,0 +1,29 @@
+// =====================================================================================
+// 
+//       Filename:  hello.cpp
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2013年01月18日 19时21分24秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Jiajie.Chen, linux.kakit@gmail.com
+//        Company:  Class 1001 of Software Engineering, XUPT
+// 
+// =====================================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello ChenHexin, and I love you !" << endl;
+	cout << "\t\t\t————Mr.Beaver" << endl;
+
+	return 0;
+}
+
+
+
